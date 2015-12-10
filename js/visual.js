35728632c2b1c93dd134a751527f40f34c699537
jy19 <j.young184@gmail.com>
2015-12-09 19:19:06 -0700

    add visualize code, not working

---
+++ js/visual.js
@@ -0,0 +1,74 @@
+var dna_visual = function()
+{
+    var flowchart = {},
+        node_width = 2 ,
+        node_height = 50 ,
+        size = [1, 1] ,
+        nodes = [];
+
+
+    flowchart.node_width = function( _ )
+    {
+        if( !arguments.length ) return node_width;
+        node_width = +_;
+        return flowchart;
+    };
+
+    flowchart.node_padding = function( _ )
+    {
+        if( !arguments.length ) return node_padding;
+        node_padding = _;
+        return flowchart;
+    };
+
+    flowchart.node_height = function( _ )
+    {
+        if( !arguments.length ) return node_height;
+        node_height = _;
+        return flowchart;
+    }
+
+    flowchart.deltax = function( _ )
+    {
+        if( !arguments.length ) return deltax;
+        deltax = _;
+        return flowchart;
+    }
+
+    flowchart.nodes = function( _ )
+    {
+        if( !arguments.length ) return nodes;
+        nodes = _;
+        return flowchart;
+    };
+
+    flowchart.size = function( _ )
+    {
+        if( !arguments.length ) return size;
+        size = _;
+        return flowchart;
+    };
+
+    flowchart.init = function() {};
+
+    flowchart.layout = function()
+    {
+        compute_node_positions();
+    };
+
+    function compute_node_positions()
+    {
+        var n = nodes.length;
+        deltax = ( size[0] - n * node_width ) / (n-1);
+        for( var i=0 ; i<n ; ++i )
+        {
+                var node = nodes[i];
+                node.x = i * ( node_width + deltax );
+                node.y = node_height;
+                node.dx = node_width;
+                node.dy = node_height;
+        }
+    }
+
+    return flowchart;
+};
--- js/visualize.js
+++ js/visualize.js
@@ -1,7 +1,7 @@
 var margin = {top: 50, right: 10, bottom:50, left: 10},
     width = $(window).width(),
     height = $(window).height(),
-    colors = [];
+    colors = {"A": "#d7191c","C": "#fdae61","G": "#ffffbf","T": "#a6d96a", "-": "#000000"};
 
 var svg1 = d3.select("#dna1").append("svg")
     .attr("width", width)
@@ -13,16 +13,41 @@ var svg2 = d3.select("#dna2").append("svg")
     .attr("width", width)
     .attr("height", height)
     .append("g")
-    .attr();
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var dna_visual = function(dna_file) {
+function get_node_color(node) {
+    return colors[node.char]
+}
+
+function visualize(dna_file, svg_elem) {
     d3.text(dna_file,
-        function(d) {
-           console.log(d);
-           return d;
-        },
-        function() {}
-    );
-};
+        function(data) {
+            console.log(data);
+            var nodes = [];
+            for (var i = 0; i < data.length; i++) {
+                var curr_node = new Node({char: data[i]});
+                nodes.push(curr_node)
+            }
 
-dna_visual("../dna_files/dna1_test");
+            var visual = dna_visual()
+                .size([width, height])
+                .nodes(nodes);
+
+            visual.layout();
+
+            // append a rect to each node
+            var node = svg_elem.append("g").selectAll(".node")
+                .data(visual.nodes)
+                .enter().append("g")
+                .attr("class", "node")
+                .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+            var rect = node.append("rect")
+                .attr("height", visual.node_height())
+                .attr("width", visual.node_width())
+                .style("fill", function(d) { return get_node_color(d); });
+        }
+    );
+}
+
+visualize("../dna_files/dna1_test", svg1);
